b49d7afe8903d5c77a1794b87f3ce82715564801
Bogna Pawlus <bogna.pawlus@gmail.com>
2022-11-14 01:03:15 +0100

    4-zadanie, poprawki

--- 4-zadanie.sh
+++ 4-zadanie.sh
@@ -10,6 +10,10 @@ grep -o '>' NC_010437.faa | wc -l
 #c)
 num=0
 
+
+#num==0 -> szukamy 'spike'
+#num==1 -> wypisujemy
+#num==2 -> koniec
 cat NC_002306.faa | while read fil
 do
 	if [[ $fil =~ 'spike' ]]
@@ -17,13 +21,14 @@ do
 		num=1
 	elif [[ $fil =~ '>' ]]
 	then
-		num=0
+		num=2
 	elif [[ $num == 1 ]]
 	then
 		echo $fil >> out1.txt
 	fi
 done 
 
+num=0
 cat NC_010437.faa | while read fil
 do
 	if [[ $fil =~ 'spike' ]]
@@ -31,7 +36,7 @@ do
 		num=1
 	elif [[ $fil =~ '>' ]]
 	then
-		num=0
+		num=2
 	elif [[ $num == 1 ]]
 	then
 		echo $fil >> out2.txt
